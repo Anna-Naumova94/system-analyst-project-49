07205ae0187117510b828df1b033fb3c16cb7a1d
Anna Naumova <anna.naumova94@icloud.com>
2026-05-16 10:25:29 +0300

    Fixed errors in calc.py

--- brain_games/games/cacl.py
+++ brain_games/games/cacl.py
@@ -16,11 +16,12 @@ def calculate(num1, num2, operator):
 
 
 def generate_round_data():
-    num1 = random.randint(1, 50)
-    num2 = random.randint(1, 20)
+    num1 = random.randint(-50, 50)
+    num2 = random.randint(-50, 50)
     operator = random.choice(['+', '-', '*'])
 
     question = f"{num1} {operator} {num2}"
     correct_answer = str(calculate(num1, num2, operator))
 
     return question, correct_answer
+
